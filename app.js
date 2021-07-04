a662c3d9c5b043b1d9385935fe08da035036762e
Prachurya <prachuryadas326@gmail.com>
2021-07-05 00:08:50 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Announcement=require('./models/announcement')
 
 // express app
 const app = express();
 
+//connect to mongodb
+const dbURI = 'mongodb+srv://Coolb:59UBSTSYY3TCJZZk@cluster0.k8t44.mongodb.net/wellfareBoard?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{useNewUrlParser:true, useUnifiedTopology: true})
+    .then((result)=> app.listen(3000))
+    .catch((err)=> console.log(err));
+
 // listen for requests
 
-app.listen(3000);
+
 // register view engine
 app.set('view engine', 'ejs');
+
 // app.set('views', 'myviews');
 
 app.use(express.static('public'));
 
-//mongoose and mongo sandbox routes
+
 
 app.use('/public/images/', express.static('./public/images'));
 
 app.get('/',(req, res)=>{
-    res.render('index',{title: 'Home'})
+    Announcement.find().sort({ creation:-1 })
+    .then((result)=>{res.render('index',{announcements: result, title: 'Home'})
+    })
+    .catch(err=>{console.log(err);
+    }); 
+   
 });
+
+app.get('/add-announce',(req,res)=>{
+    const announcement = new Announcement({
+        title:'Pikachu',
+        description:'Pikachu',
+        link:'https://en.wikipedia.org/wiki/Pikachu',
+        impotant:false,
+        category:"Pokemon",
+        date: 2021-03-20,
+    });
+
+    announcement.save()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{console.log(err);});
+})
 
 
--- models/announcement.js
+++ models/announcement.js
@@ -2,15 +2,13 @@ const mongoose =require('mongoose');
 const Schema = mongoose.Schema;
 
 const announceSchema = new Schema({
-    title: {
-        type: String,
-        required:true
-    },
-    body:{
-        type:String,
-        required: true
-    }
-},{timestamps: true});
-
-const Announce = mongoose.model('Announce',announceSchema);
-module.exports =Announce;
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    link: { type: String },
+    important: { type: Boolean },
+    category: { type: String, required: true },
+    creation: { type: Date, default: Date.now },
+    
+});
+ 
+module.exports =mongoose.model('Announcement',announceSchema);
